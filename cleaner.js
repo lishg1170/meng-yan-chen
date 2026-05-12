b9ef9d285fada2b264587783c438f421b4b80ba8
lishg1170 <74wz8tcgxv@privaterelay.appleid.com>
2026-05-12 20:22:25 +0800

    Update cleaner.js

--- cleaner.js
+++ cleaner.js
@@ -1,1 +1,51 @@
+(function () {
 
+    function cleanText(text, settings) {
+
+        if (!text) return text;
+
+        Object.entries(settings.nameFixMap)
+            .forEach(([wrong, correct]) => {
+
+                text = text
+                    .split(wrong)
+                    .join(correct);
+
+            });
+
+        settings.banListSimple.forEach(word => {
+
+            text = text
+                .split(word)
+                .join("");
+
+        });
+
+        settings.banListRegex.forEach(pattern => {
+
+            try {
+
+                text = text.replace(
+                    new RegExp(pattern, "g"),
+                    ""
+                );
+
+            } catch (e) {
+
+                console.warn(
+                    "[梦晏晨] 正则错误:",
+                    pattern
+                );
+
+            }
+
+        });
+
+        return text;
+    }
+
+    window.MengCleaner = {
+        cleanText
+    };
+
+})();
